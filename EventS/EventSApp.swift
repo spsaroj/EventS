f7629c489bd99959865d987d31e7a42cdfa47de6
Saroz <31454493+spsaroj@users.noreply.github.com>
2022-05-07 16:38:05 -0500

    Initial Commit

---
+++ EventS/EventSApp.swift
@@ -0,0 +1,17 @@
+//
+//  EventSApp.swift
+//  EventS
+//
+//  Created by Saroj Paudel on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct EventSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
